234babdaf5c310cf6ee061e1cbc7ccecb61cc02d
anatoly <anatolyostrovsky@gmail.com>
2021-11-04 01:37:14 +0200

    Instrument types

--- lib/app/models/instrument/instrument.ex
+++ lib/app/models/instrument/instrument.ex
@@ -5,11 +5,26 @@ defmodule Instrument do
   @type id :: String.t()
 
   @typedoc """
-    Currency pair of the instument
+    Ticker-like name of the instrument. For monetary instruments, a currency pair is used.
   """
   @type name :: String.t()
 
-  defstruct pub_id: nil,
+  @typedoc """
+    The underlying currency of the FX instrument
+  """
+  @type base_currency() :: String.t()
+
+  @type t :: %Instrument{
+          id: id(),
+          name: name(),
+          base_currency: base_currency(),
+          quote_currency: String.t(),
+          active: boolean(),
+          created_at: String.t(),
+          updated_at: String.t()
+        }
+
+  defstruct id: nil,
             name: nil,
             base_currency: nil,
             quote_currency: nil,
--- lib/app/models/instrument/instrument.sql
+++ lib/app/models/instrument/instrument.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS instrument(
     pub_id          TEXT DEFAULT uuid_generate_v4() UNIQUE NOT NULL,
     name            TEXT UNIQUE NOT NULL,
     quote_currency  TEXT NOT NULL,
-    -- currency instrument where dual exchange of currencies is involved
-    currency_instrument BOOLEAN NOT NULL DEFAULT FALSE,
+    -- fx instruments involve an exchange of currencies 
+    fx_instrument BOOLEAN NOT NULL DEFAULT FALSE,
     base_currency   TEXT NULL,
     active          BOOLEAN NOT NULL DEFAULT TRUE
 );
